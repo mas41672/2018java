cd8414264220126905845095571f8671f9f402eb
possardt <dirk.possardt1@freenet.de>
2018-04-29 02:01:52 +0200

    'print' & 'println' methods of object 'System.out' object

---
+++ Welcome2.java
@@ -0,0 +1,9 @@
+// Fig. 2.3: Welcome2.java
+// Printing a line of text with multiple statements.
+
+public class Welcome2 {
+	public static void main(String[] args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}
+}
