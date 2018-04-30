db09b03f6486713903f3db1be20f192fa1623eb5
possardt <dirk.possardt1@freenet.de>
2018-04-30 22:46:41 +0200

    Feat: Displaying '1 2 3 4' in terminal
    Displaying '1 2 3 4' with methods
    - println
    - 4 x prints
    - printf

---
+++ Excercise_2_14.java
@@ -0,0 +1,15 @@
+// Excercise 2.14
+// Feat: Displaying '1 2 3 4' with methods
+// println, print, print, printf
+
+public class Excercise_2_14 {
+	public static void main(String[] args){
+		System.out.println("1 2 3 4");
+		System.out.print("1 ");
+		System.out.print("2 ");
+		System.out.print("3 ");
+		System.out.print("4 ");
+		System.out.printf("%n1 2 3 4%n");
+	}
+}
+
