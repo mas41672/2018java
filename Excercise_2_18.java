994ce1af869a1f47ffe79cb2cb6cba5f35c87f5e
possardt <dirk.possardt1@freenet.de>
2018-05-02 13:24:27 +0200

    Feat: Application that displays figures
    Application that displays in the terminal:
    - box
    - oval
    - arrow
    - diamond

---
+++ Excercise_2_18.java
@@ -0,0 +1,16 @@
+// Excercise 2.18
+// Application that displays a box, an oval, an arrow and a diamond
+
+public class Excercise_2_18 {
+	public static void main(String[] args) {
+		System.out.println("*********     ***      *       *    ");
+		System.out.println("*       *   *     *   ***     * *   ");
+		System.out.println("*       *  *       * *****   *   *  ");
+		System.out.println("*       *  *       *   *    *     * ");
+		System.out.println("*       *  *       *   *   *       *");
+		System.out.println("*       *  *       *   *    *     * ");
+		System.out.println("*       *  *       *   *     *   *  ");
+		System.out.println("*       *   *     *    *      * *   ");
+		System.out.println("*********     ***      *       *    ");
+	}	
+}
