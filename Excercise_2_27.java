edc3988a1f2e4873a60ada339ab5d7ec59131331
possardt <dirk.possardt1@freenet.de>
2018-05-03 16:19:05 +0200

    Feat: generate a checkboard pattern with the standard output object
    Goal: Learn Java

---
+++ Excercise_2_27.java
@@ -0,0 +1,16 @@
+// Excercise 2.27
+// generate a checkrboard pattern of asterisks
+
+public class Excercise_2_27 {
+	public static void main(String[] args) {
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		
+	}
+}
