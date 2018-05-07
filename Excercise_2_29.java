c2f571ef91771e5ce8a27ee7ba24c9ccf0c63194
possardt <dirk.possardt1@freenet.de>
2018-05-07 07:07:37 +0200

    Feat: Print the integer value of a character
    Print character's integer value from the Unicode.
    '( )' cast operator transforms the character to int type.
    '(int)'
    
    Goal: Learn Java

---
+++ Excercise_2_29.java
@@ -0,0 +1,22 @@
+// Excercise 2.29 
+// Integer value of a character
+// %c as character format specifier
+// (int) - '( )' as cast operator
+
+public class Excercise_2_29 {
+	public static void main(String[] args) {
+		System.out.printf("The character %c has the value %d%n", 'A', (int) 'A');
+		System.out.printf("The character %c has the value %d%n", 'B', (int) 'B');
+		System.out.printf("The character %c has the value %d%n", 'C', (int) 'C');
+		System.out.printf("The character %c has the value %d%n", 'a', (int) 'a');
+		System.out.printf("The character %c has the value %d%n", 'b', (int) 'b');
+		System.out.printf("The character %c has the value %d%n", 'c', (int) 'c');
+		System.out.printf("The character %c has the value %d%n", '0', (int) '0');
+		System.out.printf("The character %c has the value %d%n", '1', (int) '1');
+		System.out.printf("The character %c has the value %d%n", '2', (int) '2');
+		System.out.printf("The character %c has the value %d%n", '$', (int) '$');
+		System.out.printf("The character %c has the value %d%n", '*', (int) '*');
+		System.out.printf("The character %c has the value %d%n", '+', (int) '+');
+		System.out.printf("The character %c has the value %d%n", '/', (int) '/');
+	}
+}
