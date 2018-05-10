61587cf4c5f079f8b223e8763d913e6ada36d95b
possardt <dirk.possardt1@freenet.de>
2018-05-10 14:32:03 +0200

    Feat: Add instance variable balance
    Changes:
    	add instance variable balance
    	add methods deposit and getBalance
    
    Goal: Learn Java

--- Account.java
+++ Account.java
@@ -1,13 +1,32 @@
-// Fig. 3.5: Account.java
-// Account class that contains a name instance variable
+// Fig. 3.8: Account.java
+// Account class with a double instance 
 // and methods to set and get its value
 
 public class Account {
 	private String name; // instance variable
+	private double balance; // instance variable
 
 	// constructor initializes name with parameter name
-	public Account(String name) { // constructor name is class name
+	public Account(String name, double balance) { // constructor name is class name
 		this.name = name;
+
+		// validate that the balance is greater than 0.0; it it's not,
+		// instance variable balance keeps keeps it's default initial value of 0.0
+		if (balance > 0.0) {
+			this.balance = balance; // assign it to instance variabele balance
+		}
+	}
+
+	// method that deposits (adds) only a valid amount to the balance
+	public void deposit(double depositAmount) {
+		if (depositAmount > 0.0) { // if the depositAmount is valid
+			balance = balance + depositAmount; // add it to the balance
+		}
+	}
+
+	// method returns the account blance
+	public double getBalance() {
+		return balance;
 	}
 
 	// method to set the name in the object
--- AccountTest.java
+++ AccountTest.java
@@ -1,15 +1,43 @@
-// Fig. 3.6: AccountTest.java
-// Using the Account constructor to initialize the name instance
-// variable at the time each Account object is created
+// Fig. 3.9: AccountTest.java
+// Inputing and outputing floating-point numbers with Account objects.
+import java.util.Scanner;
 
 public class AccountTest {
 	public static void main(String[] args) {
-		// create two Account objects
-		Account account1 = new Account("Jane Green");
-		Account account2 = new Account("John Blue");
+		Account account1 = new Account("Jane Green", 50.00);
+		Account account2 = new Account("John Blue", -7.53);
 
-		// displays the name stored in object my Account
-		System.out.printf("account1 name is: %s%n", account1.getName());
-		System.out.printf("account2 name is: %s%n", account2.getName());
+		// display initial balance of each object
+		System.out.printf("%s balance: $%.2f%n",
+			account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n",
+			account2.getName(), account2.getBalance());
+
+		// create Scanner to obtain input from command window
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter deposit amount for account1: "); // prompt
+		double depositAmount = input.nextDouble(); // obtain user input
+		System.out.printf("%nadding %.2f to account1 balance%n%n",
+			depositAmount);
+		account1.deposit(depositAmount); // add to account1's balance
+
+		// display balances
+		System.out.printf("%s balance: $%.2f%n",
+			account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n",
+			account2.getName(), account2.getBalance());
+
+		System.out.print("Enter deposit amount for account2: "); // prompt
+		depositAmount = input.nextDouble(); // obtain user input
+		System.out.printf("%nadding %.2f to account2 balance%n%n",
+			depositAmount);
+		account2.deposit(depositAmount); // add to account2 balance
+
+		// display balances
+		System.out.printf("%s balance: $%.2f%n",
+			account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n",
+			account2.getName(), account2.getBalance());
 	}
 }
