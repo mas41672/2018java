cf45c078af36c5387844884f98a34e9865f7f50b
possardt <dirk.possardt1@freenet.de>
2018-05-20 00:24:09 +0200

    Feat: Draw lines - fanning lines out
    Goal: Learn Java

--- DrawLinesController.java
+++ DrawLinesController.java
@@ -1,5 +1,5 @@
 // Fig. 4.26: DrawLinesController.java
-// Using strokeLine to connect the corners of a canvas 
+// Using strokeLine to draw the fanning
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;  // Canvas provides the drawing area
@@ -10,17 +10,22 @@ public class DrawLinesController {
     @FXML
     private Canvas canvas; // used to get the GraphicsContext
  
-    // when user presses Draw Lines button, draw two Lines in the Canvas  
+    // when user presses Draw Lines button, draw a fanning in the Canvas  
     @FXML
     void drawLinesButtonPressed(ActionEvent event) {
     	// get the GraphicsContext, which is used to draw Lines in the Canvas
     	GraphicsContext gc = canvas.getGraphicsContext2D();
 
-    	// draw line from the upper-left to lower-right corner
-    	gc.strokeLine(0, 0, canvas.getWidth(), canvas.getHeight());
-
-    	// draw line from upper-right to lower-left corner
-    	gc.strokeLine(canvas.getWidth(), 0, 0, canvas.getHeight());
+    	// draw the fanning
+    	int counter = 0;
+    	double deltaX = canvas.getHeight()/ 20;
+    	double deltaY = canvas.getWidth() / 20;
+    	
+    	while (counter <= 20) {
+    		gc.strokeLine(0, 0, canvas.getHeight() - deltaY * counter, deltaX * counter);
+    		
+    		counter++;
+    	}
     }
 
 }
