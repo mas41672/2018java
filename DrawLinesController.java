791722d81188311580282e46c313aa54c6463c89
possardt <dirk.possardt1@freenet.de>
2018-05-20 01:42:13 +0200

    Feat: Draw line art in JavaFX GUI app
    Goal: Learn Java

--- DrawLinesController.java
+++ DrawLinesController.java
@@ -1,5 +1,5 @@
 // Fig. 4.26: DrawLinesController.java
-// Using strokeLine to draw fans from all corners
+// Using strokeLine to draw line art
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;  // Canvas provides the drawing area
@@ -10,19 +10,19 @@ public class DrawLinesController {
     @FXML
     private Canvas canvas; // used to get the GraphicsContext
  
-    // when user presses Draw Lines button, draw a fanning in the Canvas  
+    // when user presses Draw Lines button, draw a line art in the Canvas  
     @FXML
     void drawLinesButtonPressed(ActionEvent event) {
     	// get the GraphicsContext, which is used to draw Lines in the Canvas
     	GraphicsContext gc = canvas.getGraphicsContext2D();
 
-    	// draw the fanning
+    	// draw the line art
     	int counter = 0;
     	double deltaX = canvas.getHeight()/ 20;
     	double deltaY = canvas.getWidth() / 20;
     	
     	while (counter <= 20) {
-    		gc.strokeLine(0, 0, canvas.getHeight() - deltaY * counter, deltaX * counter);
+    		gc.strokeLine(0, deltaY * counter, deltaX + deltaX * counter, canvas.getHeight());
     		
     		counter++;
     	}
@@ -30,7 +30,7 @@ public class DrawLinesController {
     	counter = 0;
 
     	while (counter <= 20) {
-    		gc.strokeLine(0, canvas.getHeight(), deltaX * counter, deltaY * counter);
+    		gc.strokeLine(deltaX * counter, canvas.getHeight(), canvas.getWidth() , canvas.getHeight() - deltaY - deltaY * counter);
 
     		counter++;
     	}
@@ -38,7 +38,7 @@ public class DrawLinesController {
     	counter = 0;
 
     	while (counter <= 20) {
-    		gc.strokeLine(canvas.getWidth(), 0, deltaX * counter, deltaY * counter);
+    		gc.strokeLine(canvas.getWidth(), canvas.getHeight() - deltaY * counter, canvas.getWidth() - deltaX - deltaX * counter, 0);
 
     		counter++;
     	}
@@ -46,7 +46,7 @@ public class DrawLinesController {
     	counter = 0;
 
     	while (counter <= 20) {
-    		gc.strokeLine(canvas.getWidth(), canvas.getHeight(), deltaX * counter, canvas.getHeight() - deltaY * counter);
+    		gc.strokeLine(canvas.getWidth() - deltaX * counter, 0, 0, deltaY + deltaY * counter);
 
     		counter++;
     	}
