6b4db4a5a67a6de093f5c3e3cab32f905f860f1a
possardt <dirk.possardt1@freenet.de>
2018-05-28 21:51:37 +0200

    Feat: Excercise 4.38 b) - Caculates the constant of e
    File: Excercise_4_38.java
    
    The factorial function is outsourced in a public static function.
    The program prompts the user to define the number of addition terms
    for calculating the e constant. The e iteration statement contains
    a running total variable. The e constant is of type double , so
    the type cast operator is used.
    
    Goal: Learn Java - Iteration Statement with decreasing counter.

--- Excercise_4_38.java
+++ Excercise_4_38.java
@@ -1,25 +1,36 @@
-// Excercise 4.38: Factorial
+// Excercise 4.38: b) e number
 import java.util.Scanner;
 
 public class Excercise_4_38 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
 		System.out.print(
-			"Please enter a number to calculate the factorial: ");
-		int number = input.nextInt();
-		int factorial = 1;
-
-		if ( number == 0)
-			factorial = 1;
-		else {
-			int counter = 1;
-			while ( counter <= number) {
-				factorial *= counter;
-				counter++;
-			}
+			"Please enter the terms for calculating the e term: ");
+		int terms = input.nextInt();
+		int counter = 0;
+		double e = 0;
+		while (counter < terms) {
+			e += 1/(double) factorial(counter);
+			counter++;
 		}
 
-		System.out.printf("The factorial of the number is: %d%n", factorial);
+		System.out.printf(
+			"The the e constant calculate with %d terms is: %.10f%n",
+				terms, e);
+	}
+
+	public static int factorial(int number) {
+		if (number == 0) {
+			return 1;
+		}
+		else {
+			int  counter = number;
+			int  total = 1;
+			while (1 < counter) {
+				total *= counter;
+				counter--;
+			}
+			return total;
+		}
 	}
 }
