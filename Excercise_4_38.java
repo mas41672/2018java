b38795280ce7a17ae05d26a985175c93fb0180a0
possardt <dirk.possardt1@freenet.de>
2018-05-28 23:40:45 +0200

    Feat: Excercise 4.38 c) Calculates the value of e_x
    File: Excercise_4_38.java
    
    Program calculates the value of e_x with the number of addition
    terms entered by the user. This program has two static functions
    outside the main program. The first static function 'factorial'
    calculates the factorial of the integer given. The second static
    function 'toThePower' recieves a floating-point number and an
    integer that represent the power. The body of the function
    contains a selection statement for the special case. And for all
    other cases a running total to calculate x to the power of the
    counter. The e_x expression statement is an counter-controlled
    iteration statement. This iteration statement has the
    characteristic of a shifted iteration statement.
    
    Goal: Learn Java - Two static functions outside main and at least
          one 'shifted' counter-controlled iteration statement in the
          main.

--- Excercise_4_38.java
+++ Excercise_4_38.java
@@ -1,22 +1,27 @@
-// Excercise 4.38: b) e number
+// Excercise 4.38: c) e^x function - shifted iteration
 import java.util.Scanner;
 
 public class Excercise_4_38 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print(
-			"Please enter the terms for calculating the e term: ");
+			"Please enter the terms for calculating the e^x function value: "
+				);
 		int terms = input.nextInt();
+		System.out.print(
+			"Please enter the x value for the e^x function calculation: ");
+		double x = input.nextDouble();
+
 		int counter = 0;
-		double e = 0;
+		double e_x = 0.0;
 		while (counter < terms) {
-			e += 1/(double) factorial(counter);
+			e_x += toThePower(x, counter) / (double) factorial(counter);
 			counter++;
 		}
 
 		System.out.printf(
-			"The the e constant calculate with %d terms is: %.10f%n",
-				terms, e);
+			"The the e^x function value with %d terms is: %.10f%n",
+				terms, e_x);
 	}
 
 	public static int factorial(int number) {
@@ -30,6 +35,22 @@ public class Excercise_4_38 {
 				total *= counter;
 				counter--;
 			}
+			return total;
+		}
+	}
+
+	public static double toThePower(double x, int power) {
+		if (power == 0) {
+			return 1.0;
+		}
+		else {
+			int counter = 1;
+			double total = 1;
+			while (counter <= power) {
+				total *= x;
+				counter++;
+			}
+
 			return total;
 		}
 	}
